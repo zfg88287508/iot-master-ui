598e946c7d4c80e571f8c2c4f0c1b8f17ac6df20
jason <jason@zgwit.com>
2023-01-16 10:59:13 +0800

    代码提交

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { NZ_I18N } from 'ng-zorro-antd/i18n';
 import { zh_CN } from 'ng-zorro-antd/i18n';
 import { registerLocaleData } from '@angular/common';
 import zh from '@angular/common/locales/zh';
-import { FormsModule } from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TabsComponent } from './components/tabs/tabs.component';
@@ -16,6 +16,16 @@ import { AdminComponent } from './admin/admin.component';
 import { H5Component } from './h5/h5.component';
 import {RouterModule, RouterOutlet, Routes} from "@angular/router";
 import {NzTabsModule} from "ng-zorro-antd/tabs";
+import {NzFormModule} from "ng-zorro-antd/form";
+import {NzInputModule} from "ng-zorro-antd/input";
+import {NzButtonModule} from "ng-zorro-antd/button";
+import {NzCheckboxModule} from "ng-zorro-antd/checkbox";
+import {NzMessageModule} from "ng-zorro-antd/message";
+import {NzLayoutModule} from "ng-zorro-antd/layout";
+import {NzMenuModule} from "ng-zorro-antd/menu";
+import { BrokerUsersComponent } from './broker-users/broker-users.component';
+import {NzTableModule} from "ng-zorro-antd/table";
+import {NzDividerModule} from "ng-zorro-antd/divider";
 
 registerLocaleData(zh);
 
@@ -28,6 +38,12 @@ const routes: Routes = [
   {
     path: 'admin',
     component: AdminComponent,
+    children: [
+      {
+        path: 'broker/users',
+        component: BrokerUsersComponent,
+      }
+    ]
   },
   {
     path: 'h5',
@@ -40,7 +56,7 @@ const routes: Routes = [
 ]
 
 @NgModule({
-  declarations: [AppComponent, TabsComponent, LoginComponent, PageNotFoundComponent, AdminComponent, H5Component],
+  declarations: [AppComponent, TabsComponent, LoginComponent, PageNotFoundComponent, AdminComponent, H5Component, BrokerUsersComponent],
   imports: [
     RouterModule.forRoot(routes),
     BrowserModule,
@@ -49,6 +65,16 @@ const routes: Routes = [
     BrowserAnimationsModule,
     RouterOutlet,
     NzTabsModule,
+    ReactiveFormsModule,
+    NzFormModule,
+    NzInputModule,
+    NzButtonModule,
+    NzCheckboxModule,
+    NzMessageModule,
+    NzLayoutModule,
+    NzMenuModule,
+    NzTableModule,
+    NzDividerModule,
   ],
   providers: [{ provide: NZ_I18N, useValue: zh_CN }],
   bootstrap: [AppComponent],
