9f0182cf43a068b56fa0324366c585ee5e6faeb6
jason <jason@zgwit.com>
2023-02-09 19:00:45 +0800

    网关编辑

--- src/app/broker/gateways/gateways.component.ts
+++ src/app/broker/gateways/gateways.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {NzModalService} from "ng-zorro-antd/modal";
+import {Router} from "@angular/router";
+import {RequestService} from "../../request.service";
+import {NzMessageService} from "ng-zorro-antd/message";
+import {NzTableQueryParams} from "ng-zorro-antd/table";
+import {ParseTableQuery} from "../../base/table";
 
 @Component({
   selector: 'app-gateways',
@@ -7,4 +13,58 @@ import { Component } from '@angular/core';
 })
 export class GatewaysComponent {
 
+  loading = true
+  datum: any[] = []
+  total = 1;
+  pageSize = 20;
+  pageIndex = 1;
+  query: any = {}
+
+
+  constructor(private ms: NzModalService, private router: Router, private rs: RequestService, private msg: NzMessageService) {
+    this.load();
+  }
+
+  reload() {
+    this.datum =[];
+    this.load()
+  }
+
+  load() {
+    this.loading = true
+    this.rs.post("gateway/search", this.query).subscribe(res=>{
+      this.datum = res.data;
+    }).add(()=>{
+      this.loading = false;
+    })
+  }
+
+  create() {
+    this.router.navigateByUrl("/broker/gateway/create")
+    // this.ms.create({
+    //   nzTitle: "创建网关",
+    //   nzContent: gatewayEditComponent
+    // })
+  }
+
+  delete(index: number, id: number) {
+    console.log('delete', index, id)
+    this.datum.splice(index, 1);
+    this.rs.get(`gateway/${id}/delete`).subscribe(res => {
+      this.msg.success("删除成功")
+    })
+  }
+
+  onQuery($event: NzTableQueryParams) {
+    ParseTableQuery($event, this.query)
+    this.load();
+  }
+
+  search($event: string) {
+    this.query.keyword = {
+      name: $event
+    };
+    this.query.skip = 0;
+    this.load();
+  }
 }
