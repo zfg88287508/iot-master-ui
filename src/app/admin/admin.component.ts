b1060dc7d5f4818ccbd48d09e85682d059f0d29d
EnTrouble <1498829223@qq.com>
2023-04-03 10:02:06 +0800

    当前路由bind上菜单

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,9 +1,9 @@
-import {Component} from '@angular/core';
-import {AppService} from "../app.service";
-import {OemService} from "../oem.service";
-import {Router} from "@angular/router";
-import {UserService} from "../user.service";
-import {RequestService} from "../request.service";
+import { Component } from '@angular/core';
+import { AppService } from "../app.service";
+import { OemService } from "../oem.service";
+import { Router } from "@angular/router";
+import { UserService } from "../user.service";
+import { RequestService } from "../request.service";
 
 @Component({
   selector: 'app-admin',
@@ -12,10 +12,21 @@ import {RequestService} from "../request.service";
 })
 export class AdminComponent {
   constructor(protected _as: AppService, protected os: OemService, private router: Router, private us: UserService, private rs: RequestService) {
-    localStorage.setItem("main", "/admin")
+    localStorage.setItem("main", "/admin");
+    const menuList = _as.apps;
+    for (let index = 0; index < menuList.length; index++) {
+      const item = menuList[index];
+      const { entries } = item;
+      for (let i = 0; i < entries.length; i++) {
+        const it = entries[i];
+        if (`/admin${it.path}` === location.pathname) {
+          item.open = true;
+        }
+      }
+    }
   }
 
   logout() {
-    this.rs.get("logout").subscribe(res=>{}).add(()=>this.router.navigateByUrl("/login"))
+    this.rs.get("logout").subscribe(res => { }).add(() => this.router.navigateByUrl("/login"))
   }
 }
